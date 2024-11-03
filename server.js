4b0a7f0d3bb9af6120c6b9a2751ac11c40e08655
Mateo Ascarrunz Cattoretti <mateoascarrunz@gmail.com>
2024-11-03 18:20:15 +0100

    add credential to .env

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require ("express");
 const {google} = require ("googleapis");
 const cors = require('cors');
 require('dotenv').config();
-// const credentialPath = process.env.GOOGLE_CREDENTIALS_PATH;
-const googleCredentials = {
-  type: process.env.GOOGLE_TYPE,
-  project_id: process.env.GOOGLE_PROJECT_ID,
-  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines if necessary
-  client_email: process.env.GOOGLE_CLIENT_EMAIL,
-  client_id: process.env.GOOGLE_CLIENT_ID,
-  auth_uri: process.env.GOOGLE_AUTH_URI,
-  token_uri: process.env.GOOGLE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-};
+const credentialPath = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON);
+// const googleCredentials = {
+//   type: process.env.GOOGLE_TYPE,
+//   project_id: process.env.GOOGLE_PROJECT_ID,
+//   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+//   private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines if necessary
+//   client_email: process.env.GOOGLE_CLIENT_EMAIL,
+//   client_id: process.env.GOOGLE_CLIENT_ID,
+//   auth_uri: process.env.GOOGLE_AUTH_URI,
+//   token_uri: process.env.GOOGLE_TOKEN_URI,
+//   auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+//   client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+// };
 
 const app = express();
 
@@ -28,7 +28,7 @@ app.get("/api/fetch-data", async (req, res) => {
   try {
     console.log("Request to fetch data started");
     const auth = new google.auth.GoogleAuth({
-      credentials: googleCredentials,
+      credentials: credentialPath,
       scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
 
@@ -63,7 +63,7 @@ app.post("/api/update-data", async (req, res) => {
   const invitadoStatus = req.body.invitadoStatus;
 
   const auth = new google.auth.GoogleAuth({
-    credentials: googleCredentials,
+    credentials: credentialPath,
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
 
